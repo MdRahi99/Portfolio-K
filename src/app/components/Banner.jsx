470588283d20580b4eac5e53a6640da2a46bceab
MdRahi99 <mdrahi181199@gmail.com>
2024-08-11 12:25:54 +0600

    fix

--- src/app/components/Banner.jsx
+++ src/app/components/Banner.jsx
@@ -28,54 +28,75 @@ const handleDownload = () => {
 
 const Banner = () => {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-3 items-center justify-between gap-8 py-8">
-      <div className="col-span-2">
-        <div className="flex flex-col gap-4">
-          <h1 className={`${preahvihear.className} text-3xl`}>
-            Khadija Tut Tahera
-          </h1>
-          <div className="flex items-center gap-2">
-            <h3 className={`${poppins.className} text-lg opacity-90`}>
-              UI/UX Designer
-            </h3>
-            <Image
-              className=""
-              src={logo}
-              alt="Banner Logo"
-              height="48"
-              width="24"
-            />
-          </div>
-          <p className={`${poppins.className} text-[#D3D6DA] text-justify`}>
-            Hey! I am Khadija Tut Tahera. Professional UI/UX designer. Expert in User Research, Visual design, Wireframing, Prototyping, High-Fidelity Design and collaboration with developers.
-          </p>
-        </div>
-        <div className="w-full flex items-center gap-4 lg:gap-6 mt-10">
-          <Link
-            href="/contact"
-            className={`flex p-2 rounded-lg w-full lg:w-1/3 justify-center bg-sky-600 hover:bg-sky-700 text-white ${poppins.className}`}
-          >
-            Hire Me
-          </Link>
-
-          <Link
-            href="https://drive.google.com/file/d/1O_siamb8s7LSZwxliPel93kM3xWI3MbM/view?fbclid=IwY2xjawEkcJpleHRuA2FlbQIxMAABHaeRTUEpwKQioy74RrSd61waWlQCltYs60uCNuG16zN3PLnZGLxFS2RmoA_aem_LAZxVMQCPPd08ifHui57yw"
-            target="_blank"
-            className={`lg:hidden flex px-4 text-sm py-2 w-full justify-center border-2 rounded-lg hover:bg-sky-600 hover:border-sky-600 border-white text-white ${poppins.className}`}
-          >
-            Resume
-          </Link>
-        </div>
+    <div className="flex flex-col bg-slate-800 rounded-lg items-center gap-8 p-8 mt-8 lg:mt-0">
+      <div className="flex items-center justify-between w-full mb-4">
+        <Link
+          href="/"
+          className="p-2 rounded-tl-xl rounded-br-xl border-[3px] w-11 h-11 flex items-center justify-center"
+        >
+          <h1 className="font-medium text-2xl">K</h1>
+        </Link>
+        <Link
+          href="https://drive.google.com/file/d/1O_siamb8s7LSZwxliPel93kM3xWI3MbM/view?fbclid=IwY2xjawEkcJpleHRuA2FlbQIxMAABHaeRTUEpwKQioy74RrSd61waWlQCltYs60uCNuG16zN3PLnZGLxFS2RmoA_aem_LAZxVMQCPPd08ifHui57yw"
+          target="_blank"
+          className={`hidden lg:flex px-4 text-sm py-2 justify-center border-2 rounded-lg hover:bg-sky-600 hover:border-sky-600 border-white text-white ${poppins.className}`}
+        >
+          Download Resume
+        </Link>
       </div>
+      <div className="grid grid-cols-1 lg:grid-cols-3">
+        <div className="col-span-2">
+          <div className="flex flex-col gap-4">
+            <h1 className={`${preahvihear.className} text-3xl`}>
+              Khadija Tut Tahera
+            </h1>
+            <div className="flex items-center gap-2">
+              <h3 className={`${poppins.className} text-lg opacity-90`}>
+                UI/UX Designer
+              </h3>
+              <Image
+                className=""
+                src={logo}
+                alt="Banner Logo"
+                height="48"
+                width="24"
+              />
+            </div>
+            <p
+              className={`${poppins.className} w-full lg:w-2/3 text-[#D3D6DA] text-justify`}
+            >
+              Hey! I am Khadija Tut Tahera. Professional UI/UX designer. Expert
+              in User Research, Visual design, Wireframing, Prototyping,
+              High-Fidelity Design and collaboration with developers.
+            </p>
+          </div>
+          <div className="w-full flex items-center gap-4 lg:gap-6 mt-10">
+            <Link
+              href="/contact"
+              className={`flex p-2 rounded-lg w-full lg:w-1/3 justify-center bg-sky-600 hover:bg-sky-700 text-white ${poppins.className}`}
+            >
+              Hire Me
+            </Link>
 
-      <div className="lg:flex col-span-1 hidden">
-        <Image
-          src={profileLogo}
-          alt="logo"
-          width={260}
-          height={260}
-          className="rounded-lg border"
-        />
+            <Link
+              href="https://drive.google.com/file/d/1O_siamb8s7LSZwxliPel93kM3xWI3MbM/view?fbclid=IwY2xjawEkcJpleHRuA2FlbQIxMAABHaeRTUEpwKQioy74RrSd61waWlQCltYs60uCNuG16zN3PLnZGLxFS2RmoA_aem_LAZxVMQCPPd08ifHui57yw"
+              target="_blank"
+              className={`lg:hidden flex px-4 text-sm py-2 w-full justify-center border-2 rounded-lg hover:bg-sky-600 hover:border-sky-600 border-white text-white ${poppins.className}`}
+            >
+              Resume
+            </Link>
+          </div>
+        </div>
+
+        <div className="lg:flex col-span-1 justify-end hidden">
+          <Image
+            src={profileLogo}
+            alt="logo"
+            width={260}
+            height={260}
+            className="rounded-lg border"
+          />
+        </div>
       </div>
     </div>
   );
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,15 +3,8 @@ import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import UserProvider from "./contexts/UserProvider";
-import { Poppins } from "next/font/google";
-import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
-const poppins = Poppins({
-  weight: ["400", "500", "700"],
-  subsets: ["latin"],
-  display: "swap",
-});
 
 export const metadata = {
   title: "Khadija Tut Tahera",
@@ -28,21 +21,6 @@ export default function RootLayout({ children }) {
               <Header />
             </div>
             <div className="flex flex-col gap-12 w-full lg:w-[75%] min-h-screen lg:ml-auto">
-              <div className="flex items-center justify-between">
-                <Link
-                  href="/"
-                  className="p-2 rounded-tl-xl rounded-br-xl border-[3px] w-11 h-11 flex items-center justify-center"
-                >
-                  <h1 className="font-medium text-2xl">K</h1>
-                </Link>
-                <Link
-                  href="https://drive.google.com/file/d/1O_siamb8s7LSZwxliPel93kM3xWI3MbM/view?fbclid=IwY2xjawEkcJpleHRuA2FlbQIxMAABHaeRTUEpwKQioy74RrSd61waWlQCltYs60uCNuG16zN3PLnZGLxFS2RmoA_aem_LAZxVMQCPPd08ifHui57yw"
-                  target="_blank"
-                  className={`hidden lg:flex px-4 text-sm py-2 justify-center border-2 rounded-lg hover:bg-sky-600 hover:border-sky-600 border-white text-white ${poppins.className}`}
-                >
-                  Download Resume
-                </Link>
-              </div>
               {children}
             </div>
           </div>
